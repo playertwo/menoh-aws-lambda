c705c99e36e3b6fad037bf3fdc1dbee91a543114
playertwo <842517+playertwo@users.noreply.github.com>
2018-07-27 22:50:27 +0300

    fix

---
+++ README.md
@@ -0,0 +1,25 @@
+## Build fat version of the Menoh on the Amazon Linux AMI which uses in the AWS lambda
+
+- Launch instance `amzn-ami-hvm-2017.03.1.20170812-x86_64-gp2 (ami-aa5ebdd2)`
+- `sudo yum -y install git`
+- `git clone https://github.com/playertwo/menoh-aws-lambda.git`
+- `cd menoh-aws-lambda && ./build_menoh.sh http://registrationcenter-download.intel.com/___/l_mkl_2018.3.222.tgz`
+
+Link to the static MKL archive you can get after registration https://software.intel.com/en-us/mkl
+
+
+```
+ldd ~/menoh/build/menoh/libmenoh.so
+	linux-vdso.so.1 =>  (0x00007fffcb5dd000)
+	libdl.so.2 => /lib64/libdl.so.2 (0x00007fc626516000)
+	libpthread.so.0 => /lib64/libpthread.so.0 (0x00007fc6262fa000)
+	libm.so.6 => /lib64/libm.so.6 (0x00007fc625ff7000)
+	libgcc_s.so.1 => /lib64/libgcc_s.so.1 (0x00007fc625de1000)
+	libc.so.6 => /lib64/libc.so.6 (0x00007fc625a14000)
+	/lib64/ld-linux-x86-64.so.2 (0x00005608915a0000)
+```
+
+```
+ll ~/deps.zip
+-rw-rw-r-- 1 ec2-user ec2-user 6983385 Jul 27 19:47 /home/ec2-user/deps.zip
+```
--- build_menoh.sh
+++ build_menoh.sh
@@ -1,59 +1,67 @@
 #!/bin/sh
 
-set -e
-
 MKLDNN_TAG="v0.15"
 MENOH_TAG="v1.0.3"
 PROTOBUF_TAG="v3.6.0"
 MKL_VERSION="2018.3.222"
 MKL_VERSION2="18.0.3-222-18.0.3-222"
+MKL_VERSION3="2018.3-222-2018.3-222"
+MKL_VERSION4="2018-2018.3-222"
 
+set -ev
 
 #sudo yum –y install epel-release
 sudo yum-config-manager --enable epel
-sudo yum -y install cmake3
+sudo yum -y install gcc48-c++ cmake3 libtool
 #sudo yum -y upgrade
 #sudo yum -y groupinstall "Development Tools"
 #sudo update-alternatives --set gcc /usr/bin/gcc48
 
 cd $HOME
 
-git clone --branch $PROTOBUF_TAG --depth=1 https://github.com/google/protobuf.git
-cd protobuf && ./autogen.sh && ./configure --with-pic --disable-shared && make -j4 && sudo make install && sudo ldconfig
-cd $HOME
+if [ ! -d protobuf ]; then
+  git clone --branch $PROTOBUF_TAG --depth=1 https://github.com/google/protobuf.git
+  cd protobuf && ./autogen.sh && ./configure --with-pic --disable-shared && make -j4 && sudo make install && sudo ldconfig
+  cd $HOME
+fi
 
+if [ ! -f mkl.tgz ]; then
+  curl -o mkl.tgz $1
+  tar -xzf mkl.tgz
+  cd l_mkl_$MKL_VERSION/rpm
+  sudo yum -y install intel-openmp-$MKL_VERSION2.x86_64.rpm \
+                      intel-mkl-common-$MKL_VERSION3.noarch.rpm \
+                      intel-mkl-core-rt-$MKL_VERSION3.x86_64.rpm \
+                      intel-mkl-core-$MKL_VERSION3.x86_64.rpm \
+                      intel-comp-nomcu-vars-$MKL_VERSION2.noarch.rpm \
+                      intel-mkl-doc-$MKL_VERSION4.noarch.rpm \
+                      intel-comp-l-all-vars-$MKL_VERSION2.noarch.rpm \
+                      intel-mkl-common-c-$MKL_VERSION3.noarch.rpm
 
-curl -o mkl.tgz $1
-tar -xzf mkl.tgz
-cd l_mkl_$MKL_VERSION/rpm
-sudo yum -y install intel-openmp-$MKL_VERSION2.x86_64.rpm \
-                    intel-mkl-common-$MKL_VERSION2.noarch.rpm \
-                    intel-mkl-core-rt-$MKL_VERSION2.x86_64.rpm \
-                    intel-mkl-core-$MKL_VERSION2.x86_64.rpm \
-                    intel-comp-nomcu-vars-$MKL_VERSION2.noarch.rpm \
-                    intel-mkl-doc-$MKL_VERSION2.noarch.rpm \
-                    intel-comp-l-all-vars-$MKL_VERSION2.noarch.rpm \
-                    intel-mkl-common-c-$MKL_VERSION2.noarch.rpm
+  sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/mkl/lib/intel64/*.a /usr/local/lib
+  sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/mkl/include/* /usr/local/include
+  sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/compiler/lib/intel64/*.a /usr/local/lib
+  cd $HOME
+fi
 
-sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/mkl/lib/intel64/*.a /usr/local/lib
-sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/mkl/include/* /usr/local/include
-sudo cp -r /opt/intel/compilers_and_libraries_$MKL_VERSION/linux/compiler/lib/intel64/*.a /usr/local/lib
-cd $HOME
+if [ ! -d mkl-dnn ]; then
+  git clone --branch $MKLDNN_TAG --depth=1 https://github.com/intel/mkl-dnn.git
+  cd mkl-dnn
+  git apply $HOME/menoh-aws-lambda/mkl-dnn-static.patch
+  mkdir build && cd build
+  cmake3 ..
+  make -j4 && sudo make install
+  cd $HOME
+fi
 
-git clone --branch $MKLDNN_TAG --depth=1 https://github.com/intel/mkl-dnn.git
-cd mkl-dnn
-git apply $HOME/menoh-aws-lambda/mkl-dnn-static.patch
-mkdir build && cd build
-cmake3 ..
-make -j4 && sudo make install
-cd $HOME
-
-sudo yum -y install gcc64-c++
-git clone --branch $MENOH_TAG --depth=1 https://github.com/pfnet-research/menoh.git
-cd mkl-dnn
-git apply $HOME/menoh-aws-lambda/menoh-static.patch
-mkdir build && cd build
-cmake3 -DENABLE_EXAMPLE=0 -DENABLE_TOOL=0 ..
-make -j4
-zip -9 -q -r ~/deps.zip menoh/libmenoh.so
-cd $HOME
+if [ ! -d menoh ]; then
+  sudo yum -y install gcc64-c++
+  git clone --branch $MENOH_TAG --depth=1 https://github.com/pfnet-research/menoh.git
+  cd menoh
+  git apply $HOME/menoh-aws-lambda/menoh-static.patch
+  mkdir build && cd build
+  cmake3 -DENABLE_EXAMPLE=0 -DENABLE_TOOL=0 ..
+  make -j4
+  zip -9 -q -r ~/deps.zip menoh/libmenoh.so
+  cd $HOME
+fi
