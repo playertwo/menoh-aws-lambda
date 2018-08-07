1d1cf30d0e5633d0936a957d4cc189fe203f8409
playertwo <842517+playertwo@users.noreply.github.com>
2018-08-07 20:27:52 +0300

    add test

--- build_menoh.sh
+++ build_menoh.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-MKLDNN_TAG="v0.15"
+MKLDNN_TAG="master"
+MKLDNN_COMMIT="a46b870"
 MENOH_TAG="v1.0.3"
 PROTOBUF_TAG="v3.6.0"
+
+# MKL_VERSION="2018.2.199"
+# MKL_VERSION2="18.0.2-199-18.0.2-199"
+# MKL_VERSION3="2018.2-199-2018.2-199"
+# MKL_VERSION4="2018-2018.2-199"
+
 MKL_VERSION="2018.3.222"
 MKL_VERSION2="18.0.3-222-18.0.3-222"
 MKL_VERSION3="2018.3-222-2018.3-222"
 MKL_VERSION4="2018-2018.3-222"
 
+JOBS="8"
+
+TEST=true
+
 set -ev
 
-#sudo yum –y install epel-release
 sudo yum-config-manager --enable epel
-sudo yum -y install gcc48-c++ cmake3 libtool
-#sudo yum -y upgrade
-#sudo yum -y groupinstall "Development Tools"
-#sudo update-alternatives --set gcc /usr/bin/gcc48
+sudo yum -y install gcc64-c++ cmake3 libtool
+
 
 cd $HOME
 
 if [ ! -d protobuf ]; then
   git clone --branch $PROTOBUF_TAG --depth=1 https://github.com/google/protobuf.git
-  cd protobuf && ./autogen.sh && ./configure --with-pic --disable-shared && make -j4 && sudo make install && sudo ldconfig
+  cd protobuf && ./autogen.sh && ./configure --with-pic --disable-shared && make -j$JOBS && sudo make install && sudo ldconfig
   cd $HOME
 fi
 
@@ -47,21 +55,32 @@ fi
 if [ ! -d mkl-dnn ]; then
   git clone --branch $MKLDNN_TAG --depth=1 https://github.com/intel/mkl-dnn.git
   cd mkl-dnn
+  git checkout $MKLDNN_COMMIT
   git apply $HOME/menoh-aws-lambda/mkl-dnn-static.patch
   mkdir build && cd build
   cmake3 ..
-  make -j4 && sudo make install
+  make -j$JOBS && sudo make install
+  if $TEST ; then make test ; fi
   cd $HOME
 fi
 
 if [ ! -d menoh ]; then
-  sudo yum -y install gcc64-c++
   git clone --branch $MENOH_TAG --depth=1 https://github.com/pfnet-research/menoh.git
+  #git clone --branch skip_flatten --depth=1 https://github.com/playertwo/menoh.git
   cd menoh
+  if $TEST ; then
+    mkdir data
+    export PATH=$PATH:/usr/local/bin
+    sudo env "PATH=$PATH" pip install --upgrade pip
+    sudo env "PATH=$PATH" pip install chainer
+    python gen_test_data.py
+  fi
+
   git apply $HOME/menoh-aws-lambda/menoh-static.patch
   mkdir build && cd build
-  cmake3 -DENABLE_EXAMPLE=0 -DENABLE_TOOL=0 ..
-  make -j4
+  cmake3 -DENABLE_EXAMPLE=0 -DENABLE_TOOL=0 -DENABLE_TEST=1 ..
+  make -j$JOBS
   zip -9 -q -r ~/deps.zip menoh/libmenoh.so
+  if $TEST ; then ./test/menoh_test ; fi
   cd $HOME
 fi
